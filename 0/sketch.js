594fe644f707cdd00d86f8e869fa0b908e40a15a
potuitou <qpeng@risd.edu>
2024-11-18 16:54:24 -0500

    final

--- 0/sketch.js
+++ 0/sketch.js
@@ -1,12 +1,73 @@
+let slider;
+let song;
+let mFFT;
+
+function preload() {
+  song = loadSound("./Yellow_Submarin_1min.m4a");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  slider = createSlider(50, 300, 150); 
+  slider.position(10, 10);
+  slider.size(120);
+
+  mFFT = new p5.FFT();
 }
 
 function draw() {
   background(255);
-  fill(0);
-  rect(width / 2.5, height / 4, width / 20, height / 2);
-  rect(width - width / 2.5 - width / 20, height / 4, width / 20, height / 2);
-  rect(width / 2.5 + width / 20, height / 4 - width / 20, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
-  rect(width / 2.5 + width / 20, height / 4 + height / 2, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
+
+  let SUBMARINESIZE = slider.value();
+
+  mFFT.analyze();
+
+  let energyH = mFFT.getEnergy("highMid");
+  let diamH = map(energyH, 0, 255, 10, 60);
+
+  let energyM = mFFT.getEnergy("mid");
+  let diamM = map(energyM, 0, 255, 10, 60);
+
+  let energyB = mFFT.getEnergy("bass");
+  let diamB = map(energyB, 0, 255, 10, 60);
+
+  drawSubmarine(width / 2, height / 2, SUBMARINESIZE, diamH, diamM, diamB);
+}
+
+function drawSubmarine(x, y, size, diamH, diamM, diamB) {
+  push(); 
+  translate(x, y); 
+  scale(size / 10); 
+
+  fill(255, 204, 0); 
+  noStroke();
+  ellipse(0, 0, 200, 100); 
+
+  rectMode(CENTER);
+  fill(255, 204, 0);
+  rect(0, -50, 60, 40); 
+
+  stroke(0, 100, 200,160);
+  ellipse(-50, 0, diamB, diamB); // Left 
+  ellipse(0, 0, diamM, diamM);   // Center 
+  ellipse(50, 0, diamH, diamH);  // Right
+
+  fill(200, 80, 10);
+  ellipse(-50, 0, 32); 
+  ellipse(0, 0, 32);   
+  ellipse(50, 0, 32);
+
+  noStroke();
+  fill(25);
+  ellipse(110, 0, 20, 40); 
+
+
+  pop();
+}
+
+function mouseClicked() {
+  if (!song.isPlaying()) {
+    song.play();
+  }
 }
--- 1/sketch.js
+++ 1/sketch.js
@@ -1,11 +1,108 @@
+let slider;
+let song;
+let samples;
+
+function preload() {
+  song = loadSound("Yellow_Submarine_Full.mp3");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  slider = createSlider(50, 300, 20); 
+  slider.position(10, 10);
+  slider.size(120);
+
+  samples = song.getPeaks();
 }
 
 function draw() {
   background(255);
-  fill(0);
-  rect(width / 2 - width / 40, height / 4 - width / 20, width / 20, height / 2 + width / 10);
-  rect(width / 2.5, height / 4 + height / 2, width - 2 * width / 2.5, width / 20);
-  rect(width / 2 - width / 20 - width / 40, height / 4 - width / 20, width / 20, width / 20);
+
+  let SUBMARINESIZE = slider.value();
+
+  drawAudioBubbles();
+  drawSubmarine(width / 2, height / 2, SUBMARINESIZE);
+}
+
+function drawAudioBubbles() {
+  for (let idx = 0; idx < samples.length; idx += int(samples.length / 200)) {
+    let x = map(idx, 0, samples.length - 1, 0, width);
+    let y = map(samples[idx], -1, 1, 0, height);
+    let size = map(samples[idx], -1, 1, 10, 70); // size 
+    draw3DBubble(x, y, size);
+  }
+}
+
+function draw3DBubble(x, y, size) {
+  push();
+  translate(x, y);
+
+  // Shadow
+  noStroke();
+  fill(50, 50, 50, 80);
+  ellipse(5, 5, size * 0.9, size * 0.9);
+
+  // Main Bubble
+  noStroke();
+  fill(0, 100, 200);
+  ellipse(0, 0, size, size);
+
+  // Highlight
+  fill(255, 255, 255, 180);
+  ellipse(-size / 4, -size / 4, size / 5, size / 5);
+
+  pop();
+}
+
+function drawSubmarine(x, y, size) {
+  push(); 
+  translate(x+15, y+15); 
+  scale(size / 10); 
+
+  fill(50, 50, 50, 80);
+  noStroke();
+  ellipse(0, 0, 200, 100); 
+
+  rectMode(CENTER);
+  fill(50, 50, 50, 80);
+  rect(0, -50, 60, 40); 
+
+  fill(50, 50, 50, 80);
+  ellipse(-50, 0, 32); 
+  ellipse(0, 0, 32);   
+  ellipse(50, 0, 32);
+
+  fill(50, 50, 50, 80);
+  ellipse(110, 0, 20, 40); 
+
+  pop();
+
+  push(); 
+  translate(x, y); 
+  scale(size / 10); 
+
+  fill(255, 204, 0); 
+  noStroke();
+  ellipse(0, 0, 200, 100); 
+
+  rectMode(CENTER);
+  fill(255, 204, 0);
+  rect(0, -50, 60, 40); 
+
+  fill(0, 100, 200, 160);
+  ellipse(-50, 0, 32); 
+  ellipse(0, 0, 32);   
+  ellipse(50, 0, 32);
+
+  fill(150);
+  ellipse(110, 0, 20, 40); 
+
+  pop();
+}
+
+function mouseClicked() {
+  if (!song.isPlaying()) {
+    song.play();
+  }
 }
